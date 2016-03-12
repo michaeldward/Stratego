8c02440070ec282d363461e031b5d24151c1a4f7
Michael <xenophon.of.athens@gmail.com>
2016-03-12 12:57:13 -0700

    basic GUI start

--- GUI.java
+++ GUI.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+
 /**
  * Created by Michael on 3/12/2016.
  */
-public class GUI {
+public class GUI extends JFrame //implements ActionListener, KeyListener
+{
+
+    public GUI()
+    {
+        super();
+        this.setLayout(null);
+        this.setVisible(true);
+        this.setSize(500, 500);
+        this.setResizable(false);
+        this.setTitle("Stratego");
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
+
+    public static void main(String[] args)
+    {
+        GUI g = new GUI();
+    }
 }
