13ce21f11746c3491063fb7f7b1dd139da1780a6
Michael <xenophon.of.athens@gmail.com>
2016-03-12 13:00:52 -0700

    added a button to test

--- GUI.java
+++ GUI.java
@@ -8,6 +8,8 @@ import java.awt.event.KeyListener;
 public class GUI extends JFrame //implements ActionListener, KeyListener
 {
 
+    private JButton testButton;
+
     public GUI()
     {
         super();
@@ -17,6 +19,10 @@ public class GUI extends JFrame //implements ActionListener, KeyListener
         this.setResizable(false);
         this.setTitle("Stratego");
         this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        testButton = new JButton("Test");
+        testButton.setSize(100, 50);
+        testButton.setLocation(200, 100);
+        this.getContentPane().add(testButton);
     }
 
     public static void main(String[] args)
