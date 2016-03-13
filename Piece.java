bfef2e7d887084c14a079dbee0af3ce026630e4c
Michael <xenophon.of.athens@gmail.com>
2016-03-13 10:47:12 -0600

    changed switch to if-else

--- Piece.java
+++ Piece.java
@@ -1,3 +1,5 @@
+import java.net.URL;
+
 /**
  * Created by Michael on 3/13/2016.
  */
@@ -9,47 +11,68 @@ public class Piece
     private PieceType rank;
     private int speed = 1;
     private boolean disarm = false;
+    private String imageName;
     public Piece(PieceType type)
     {
         rank = type;
-        switch(type)
+        if (type == PieceType.Marshal)
         {
-            case Marshal:
-                value = 10;
-                break;
-            case General:
-                value = 9;
-                break;
-            case Colonel:
-                value = 8;
-                break;
-            case Major:
-                value = 7;
-                break;
-            case Captain:
-                value = 6;
-                break;
-            case Lieutenant:
-                value = 5;
-                break;
-            case Sergeant:
-                value = 4;
-                break;
-            case Miner:
-                value = 3;
-                disarm = true;
-                break;
-            case Scout:
-                value = 2;
-                speed = 10;
-                break;
-            case Flag:
-                value = 1;
-                speed = 0;
-                break;
-            case Bomb:
-                value = 11;
-                speed = 0;
+            value = 10;
+            imageName = "marshal.png";
+        }
+        else if (type == PieceType.General)
+        {
+            value = 9;
+            imageName = "general.png";
+        }
+        else if (type == PieceType.Colonel)
+        {
+            value = 8;
+            imageName = "colonel.png";
+        }
+        else if (type == PieceType.Major)
+        {
+            value = 7;
+            imageName = "major.png";
+        }
+        else if (type == PieceType.Captain)
+        {
+            value = 6;
+            imageName = "captain.png";
+        }
+        else if (type == PieceType.Lieutenant)
+        {
+            value = 5;
+            imageName = "lieutenant.png";
+        }
+        else if (type == PieceType.Sergeant)
+        {
+            value = 4;
+            imageName = "sergeant.png";
+        }
+        else if (type == PieceType.Miner)
+        {
+            value = 3;
+            imageName = "miner.png";
+            disarm = true;
+        }
+        else if (type == PieceType.Scout)
+        {
+            value = 2;
+            imageName = "scout.png";
+            speed = 10;
+        }
+        else if (type == PieceType.Bomb)
+        {
+            value = 11;
+            imageName = "bomb.png";
+            speed = 0;
+        }
+        else if (type == PieceType.Flag)
+        {
+            value = 1;
+            imageName = "flag.png";
+            speed = 0;
         }
     }
 }
